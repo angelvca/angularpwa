a499bc244044698550670c218de9967c25c33ca2
angelvca <angelvca16@gmail.com>
2025-02-19 20:26:44 -0600

    Configuración en el angular, ngsw y main

--- angular-pwa-app/src/main.ts
+++ angular-pwa-app/src/main.ts
@@ -2,5 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js').then((registration) => {
+      console.log('Service worker registered:', registration);
+    }).catch((error) => {
+      console.error('Service worker registration failed.', error);
+    })
+  });
+}
+
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch(err => console.error('Error al iniciar la aplicación: ', err));
